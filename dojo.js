92b9c7a5c2fa6c811d5fcff884aec786a8ba51a6
rahulmishra <rahulmishra.hello@gmail.com>
2026-04-06 01:53:18 +0530

    added

---
+++ dojo.js
@@ -0,0 +1,16 @@
+const inp = "google";
+
+switch (term) {
+  case "google":
+    console.log("google");
+    break;
+  case "google":
+    console.log("google");
+    break;
+  case "google":
+    console.log("google");
+    break;
+  case "google":
+    console.log("google");
+    break;
+}
